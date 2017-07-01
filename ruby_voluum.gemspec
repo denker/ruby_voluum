5854bf9d9d7e338c77334be9123f1aab1a146efd
Tim Zuev <tim.zuev@gmail.com>
2017-07-01 19:22:24 +0300

    test suite setup

--- ruby_voluum.gemspec
+++ ruby_voluum.gemspec
@@ -32,5 +32,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.14'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.5'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
   spec.add_development_dependency 'pry'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
-require "bundler/setup"
-require "ruby_voluum"
+require 'bundler/setup'
+require 'ruby_voluum'
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/fixtures'
+  c.hook_into :webmock
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
 end

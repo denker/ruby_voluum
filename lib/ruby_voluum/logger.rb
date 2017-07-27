adf520adf56a2ed57eadc83fc154095429e6de84
Kravchuk <majerkravchuk@gmail.com>
2017-07-27 17:11:03 +0300

    Fix Logger class: make the methods static

--- lib/ruby_voluum/logger.rb
+++ lib/ruby_voluum/logger.rb
@@ -1,36 +1,36 @@
 module RubyVoluum
   class Logger
-    def initialize(path = nil)
-      @path = File.exist?(path.to_s) ? path : nil
-    end
+    class << self
+      attr_accessor :log_file_path
 
-    def log(response, request, result)
-      request_date = DateTime.now.strftime('%d-%m-%Y %H:%M')
-      request_info = "#{request.method.upcase} #{request.url}"
-      response_info = result.code.to_s
+      def log(response, request, result)
+        request_date = DateTime.now.strftime('%d-%m-%Y %H:%M')
+        request_info = "#{request.method.upcase} #{request.url}"
+        response_info = result.code.to_s
 
-      if ('200'..'299').exclude?(result.code)
-        response_message = result.message
-        description = error_description(response)
-        response_message << ": #{description}" if description
-        response_info = "#{response_info} `#{response_message}`"
+        if ('200'..'299').exclude?(result.code)
+          response_message = result.message
+          description = error_description(response)
+          response_message << ": #{description}" if description
+          response_info = "#{response_info} `#{response_message}`"
+        end
+
+        message = "[#{request_date}] -- \"#{request_info}\" | #{response_info}"
+
+        File.open(log_file_path, 'a') { |f| f.write "#{message}\n" } if log_file_path
+
+        message = "  VoluumClient: #{message}"
+        message = ('200'..'299').cover?(result.code) ? message.green : message.red
+        puts message
       end
 
-      message = "[#{request_date}] -- \"#{request_info}\" | #{response_info}"
+      private
 
-      File.open(@path, 'a') { |f| f.write "#{message}\n" } if @path
-
-      message = "  VoluumClient: #{message}"
-      message = ('200'..'299').cover?(result.code) ? message.green : message.red
-      puts message
-    end
-
-    private
-
-    def error_description(response)
-      JSON.parse(response.body)['error']['description']
-    rescue
-      nil
+      def error_description(response)
+        JSON.parse(response.body)['error']['description']
+      rescue
+        nil
+      end
     end
   end
 end
